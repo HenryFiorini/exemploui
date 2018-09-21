3084339e1ed0988620b45f44e92712556fc6edad
Marco Mangan <marco.mangan@gmail.com>
2018-09-21 16:06:09 -0300

    Exemplos com código de Page Object

--- uitest/src/test/java/pucrs/s2b/uitest/po/MetricConversionPage.java
+++ uitest/src/test/java/pucrs/s2b/uitest/po/MetricConversionPage.java
@@ -1,5 +1,22 @@
 package pucrs.s2b.uitest.po;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
 public class MetricConversionPage {
+
+	@FindBy(id = "argumentConv1")
+	@CacheLookup
+	WebElement argumentConv1;
+
+	@FindBy(id = "answer")
+	@CacheLookup
+	WebElement answer;
+
+	void fillMiles(double x) {
+		argumentConv1.sendKeys(String.valueOf(x));
+		argumentConv1.submit();
+	}
 
 }
--- uitest/src/test/java/pucrs/s2b/uitest/po/MetricConversionTest.java
+++ uitest/src/test/java/pucrs/s2b/uitest/po/MetricConversionTest.java
@@ -1,17 +1,14 @@
 package pucrs.s2b.uitest.po;
 
 import static org.junit.Assert.assertEquals;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.PageFactory;
 
 /**
  * @see https://github.com/s2b-teste-2018-2/webdrivermanager-examples/blob/master/src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
@@ -21,49 +18,55 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class MetricConversionTest {
 
 	private WebDriver driver;
+	private MetricConversionPage page;
 
 	@BeforeClass
 	public static void setupClass() {
-		System.setProperty("webdriver.chrome.driver", 
-				"C:\\Users\\teste\\chromedriver_win32\\chromedriver.exe");
-	}	
-	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\teste\\chromedriver_win32\\chromedriver.exe");
+	}
+
 	@Before
 	public void setupTest() {
 		driver = new ChromeDriver();
-	}
-	
-    @After
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-	
-	@Test
-	public void givenGooglePageWhenSearchingForWikipediaThenAWikipediaSearchBoxAppears() {
+
 		String initialPage = "https://www.metric-conversions.org/pt-br/comprimento/milhas-em-quilometros.htm";
 		driver.get(initialPage);
-		
-		WebDriverWait wait = new WebDriverWait(driver, 30);
-		
-		By argumentConv1 = By.id("argumentConv1");
-		wait.until(presenceOfElementLocated(argumentConv1));
-		WebElement inputElement = driver.findElement(argumentConv1);
-		
-		inputElement.sendKeys("10");
-		inputElement.submit();
-		
-		By answer = By.id("answer");
-		wait.until(presenceOfElementLocated(answer));
-		WebElement outElement = driver.findElement(answer);
-		
-		double actual = Double.parseDouble(outElement.getText().split("=")[1].replace("km",""));
-		double expected = 16.09;
-		
-		assertEquals(expected, actual, .01);
+
+		page = PageFactory.initElements(driver, MetricConversionPage.class);
+
 	}
 
+	@After
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 
+	@Test
+	public void givenXMilesWhenConvertingToKilometersThenAnswerIsYKilometers() {
+		double x = 10.0; // X
+		page.fillMiles(x);
+
+		String output = page.answer.getText();
+		
+		double actual = Double.parseDouble(output.split("=")[1].replace("km", ""));
+		double expected = 16.09; // Y
+
+		assertEquals(expected, actual, .01);
+	}
+	
+	@Test
+	public void givenXMilesWhenConvertingToKilometersThenAnswerIsYKilometersAgain() {
+		double x = 0.0; // X
+		page.fillMiles(x);
+
+		String output = page.answer.getText();
+		
+		double actual = Double.parseDouble(output.split("=")[1].replace("km", ""));
+		double expected = 0.0; // Y
+
+		assertEquals(expected, actual, .01);
+	}	
 
 }
